54fe427c6ec9ac82e0670e1e2405f10ee87bca0e
Suraj Shetty <surajshetty2804@gmail.com>
2024-05-17 13:38:55 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="container">
+<hr>
+<p class="centered">Created by <a href=""><b>Suraj Shetty & Vijeth</b></a>  <b>Water billing</b></p>
+</div>
---
+++ user/footer.php
@@ -0,0 +1,8 @@
+<!-- FOOTER -->
+    <!--<footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                Created by <a href="">Suraj Shetty & Vijeth </a>  Water Billing System
+            </div>
+        </div>
+    </footer>
